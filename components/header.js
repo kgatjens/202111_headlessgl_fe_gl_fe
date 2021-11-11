900cd3d5b5dccacfd2a4e518f30f97ca0d62a4ad
kbrenes <contact@kennethbrenes.com>
2021-11-10 21:50:04 -0600

    removing strokes tags

--- components/header.js
+++ components/header.js
@@ -62,7 +62,7 @@ export default function Header(headerData) {
       <a className="navbar-burger self-center mr-12 xl:hidden" href="#">
           {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-          </svg> */}
+          </svg> */} 
       </a>
         
             { headerData.header.menuItems.nodes.length ? headerData.header.menuItems.nodes.map( menuItem => (
