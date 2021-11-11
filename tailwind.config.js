ea0a3a11c85d9e8eb7d1c180312c49c06daf8d05
kbrenes <contact@kennethbrenes.com>
2021-11-10 21:38:55 -0600

    Adding tailwing base config, colors and main nav section

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        black: '#000',
+        white: '#fff',
+        red: 'red',
+        lightblue:'#3966ad',
+        darkblue:'#233c77',
+        orange:'#ECBC36',
+        darkorange:'#ec9422',
+        green:'#8bb63f',
+        purple: '#71445B',
+        lightblue2:'#1293CD',
+        gray: '#4A4A4A',
+        gray2: '#979797',
+      },
+      gradientColorStops: theme => ({
+        'primary': '#FF8C00',
+        'secondary': '#FFA500',
+        'danger': '#FFD700',
+      }),
+    },
+    
+    fontFamily: {
+      sans: ['Open Sans', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
