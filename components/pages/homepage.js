0b35779ea5cf9f5ce02c5a22794f6ce2c183b182
kbrenes <contact@kennethbrenes.com>
2022-03-11 17:29:06 -0600

    Added component for cover image, also data passed by variable to components

--- components/pages/homepage.js
+++ components/pages/homepage.js
@@ -1,21 +1,36 @@
 import Link from 'next/link';
+import CoverImage from '../layout/cover-image'
 
-export default function Homepage(allHomepage) {
-    console.log("**Homepage**");
-    const data = allHomepage.homepage.edges[0];
-    console.log(data);
-    console.log("**");
+export default function Homepage({
+    title,
+    descriptionText,
+    headerImageURL,
+    headerImageAltText,
+    sectionBackgrounURL,
+    sectionBackgrounAltText,
+    sectionColor,
+  }) {
     return(
         <div>
-            <h1>{data.node.homepageFields.homepageTitle}</h1>
-            <div>
-                <p>{data.node.homepageFields.initialDescriptionText}</p>
-                <p>{data.node.homepageFields.sectionBackgroundImage.altText}</p>
-                <p>{data.node.homepageFields.sectionBackgroundImage.mediaItemUrl}</p>
-                <p>{data.node.homepageFields.sectionColor}</p>
-
-            </div>
+        <div className="mb-8 md:mb-16">
+          {headerImageURL && (
+            <CoverImage title={headerImageAltText} coverImage={headerImageURL}  />
+          )}
         </div>
+        <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
+          <div>
+            <h1 className="mb-4 text-4xl lg:text-6xl leading-tight" 
+                dangerouslySetInnerHTML={{ __html: title }}>
+            </h1>
+          </div>
+          <div>
+            <div
+              className="text-lg leading-relaxed mb-4"
+            //   dangerouslySetInnerHTML={{ __html: excerpt }}
+            />
+          </div>
+        </div>
+      </div>
         
     )
 }
