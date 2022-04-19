b3891eb62bba01038a680259c791dacc6dd54c54
Kenneth Brenes <kenneth.brenes@Kenneth-Brenes-MacBook-Pro.local>
2022-04-19 16:42:35 -0600

    Added components for render more blogs in blog landing page

--- pages/blogs.js
+++ pages/blogs.js
@@ -5,6 +5,8 @@ import Container from '../components/layout/container'
 import Layout from '../components/layout/layout'
 import Header from '../components/layout/header'
 import Footer from '../components/layout/footer'
+import MoreBlogs from '../components/pages/more-posts'
+
 
 
 import { getFirstBlogs } from '../lib/wp/api'
@@ -16,8 +18,11 @@ export default function Blogs({ menus , firstBlogs }) {
   const pageTitle = "GL - Blogs";
   const { mainNav, mainFooter } = menus || {};
   const blogs = firstBlogs.edges
+  const morePosts = blogs.slice(1)
+
 
   console.log(blogs);
+  console.log(morePosts);
   return (
     <>
       <Layout>
@@ -26,6 +31,7 @@ export default function Blogs({ menus , firstBlogs }) {
       <Container>
         <h1>Available Blogs</h1>
         
+        {morePosts.length > 0 && <MoreBlogs posts={morePosts} />}
 
       </Container>
 
