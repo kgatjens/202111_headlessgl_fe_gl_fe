060558021ad9495deee9c1208f591381262526bc
Kenneth Brenes <kenneth.brenes@Kenneth-Brenes-MacBook-Pro.local>
2022-05-13 15:46:09 -0600

    Added carousel test

--- components/lib/carousel.js
+++ components/lib/carousel.js
@@ -1,29 +1,40 @@
 import React, { Component } from 'react';
-//import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { CarouselComponent } from 'react-responsive-carousel';
-
-
-//export default class NextJsCarousel extends Component {
-
-export default function Carousel({ carousel }) {
-     console.log(carousel)
-    // console.log("**#*")
-    // console.log(Array.isArray(carousel))
-    // console.log("***")
-
-  return (
-    <section>
-      <h4>Carousel</h4>
-     
-      {Object.keys(carousel.carouselFields).map((index)  => (
-          
-          <span key={index} className="ml-4 font-normal">
-            {carousel.carouselFields[index].title}
-            <img src={carousel.carouselFields[index].sourceUrl} />
-            
-          </span>
-        ))}
-         
-    </section>
-  )
-}
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+  
+export default class NextJsCarousel extends Component {
+    render({ carousel1 }) {
+        return (
+            <div>
+              <h2>NextJs Carousel - </h2>
+              <Carousel>
+                  <div>
+                      <img src="/1.png" alt="image1"/>
+                      <p className="legend">Image 1</p>
+  
+                  </div>
+                  <div>
+                      <img src="/2.png" alt="image2" />
+                      <p className="legend">Image 2</p>
+  
+                  </div>
+                  <div>
+                      <img src="/3.png" alt="image3"/>
+                      <p className="legend">Image 3</p>
+  
+                  </div>
+                  <div>
+                      <img src="/4.png" alt="image4"/>
+                      <p className="legend">Image 4</p>
+  
+                  </div>
+                  <div>
+                      <img src="/5.png" alt="image5"/>
+                      <p className="legend">Image 5</p>
+  
+                  </div>
+              </Carousel>
+            </div>
+        );
+    }
+};
