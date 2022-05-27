6a4b760df2526dfadf0c63f1fe2bc022cab634da
Kenneth Brenes <kenneth.brenes@Kenneth-Brenes-MacBook-Pro.local>
2022-05-27 14:42:31 -0600

    Keep working on load more, finally the event is doing the query, now the state should add the data and rendere it

--- pages/blogs2.js
+++ pages/blogs2.js
@@ -7,14 +7,49 @@ import Header from '../components/layout/header'
 import Footer from '../components/layout/footer'
 import MoreBlogs from '../components/pages/more-posts'
 
-import useSWR, { useSWRConfig } from "swr";
+import useSWR from "swr";
 import { useEffect, useState } from 'react';
 import axios from "axios";
 
 import { getFirstBlogs, getMenus } from '../lib/wp/api'
-import { data } from 'autoprefixer';
-//GOAL HERE, TRY IT WITH MUTATIONS TO GET DATA ON THE SAME COMPONENT
 
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+const loadedMoreData = {};
+
+const MoreLoader = ({ value, perPage }) => {
+  const url = `https://headlessgl22.wpengine.com/wp-json/wp/v2/posts/?status=publish&per_page=${perPage}&offset=${value}&orderby=date&order=desc`
+  const { data, error } = useSWR(
+    url,
+    fetcher
+  );
+  
+  // console.log("ˆˆˆ1ˆˆˆˆ");
+  // console.log(value);
+  // console.log(perPage);
+  // console.log(data);
+  // console.log(error);
+  // console.log("ˆˆˆˆ2ˆˆˆ");
+
+  loadedMoreData = { ...loadedMoreData, ...data } 
+
+  const [loadedData, setloadedData] = useState(loadedMoreData);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
+  if (!data[0]) return <div>not found</div>;
+  const html = <ul>
+            {data.map((tag, index) => (
+              <li key={index} className="ml-4 font-normal">
+                <p>{tag.title.rendered}</p>
+              </li>
+            ))}
+              </ul>;
+
+  return html;
+};
 
 export default function Blogs({ menus , firstBlogs }) {
   
@@ -32,19 +67,26 @@ export default function Blogs({ menus , firstBlogs }) {
 
   const loadPerPage = 6;
 
+  const [startFetching, setStartFetching] = useState(false);
+  const [indexValue, setIndexValue] = useState(0);
+  const loadedMoreData2 = { ...loadedMoreData, ...loadedMoreData } 
+  const [loadedData, setloadedData] = useState(loadedMoreData2);
+
+  const handleClick = () => {
+    setStartFetching(true);
+    setIndexValue(indexValue+loadPerPage);
+    setloadedData(loadedData);
+
+    // console.log('increment like count');
+    // console.log(indexValue+" = "+loadPerPage);
+  };
+
+  // console.log("ˆˆˆ*WWW*ˆˆˆˆ");
+  // console.log(loadedData);
+  // console.log(loadedMoreData2);
+  // console.log("ˆˆˆ**WWW2*ˆˆˆˆ");
   
   
-  const loadMore = async () => {
-    const { mutate } = useSWRConfig()
-
-    const { data } = useSWR('https://headlessgl22.wpengine.com/wp-json/wp/v2/posts/?status=publish&per_page=6&offset=6&orderby=date&order=desc',
-    (...args) => fetch(...args).then((res) => res.json()))
-  }
-
-  console.log(data)
-  if (!data) return <p>loading</p>
- 
-
   const metaData = {metaTitle,featuredImage,metaKeywords,metaDesc,canonical}
   return (
     <>
@@ -62,10 +104,19 @@ export default function Blogs({ menus , firstBlogs }) {
             
 
       </Container>
-      
-        <button 
-        className='className="flex items-center cursor-pointer	bg-gray-100 hover:bg-gray-600 hover:text-white transition-colors duration-500 border border-gray-500 px-4 py-3"'
-        onClick={() => loadMore()}>Load More</button>
+      {startFetching && <MoreLoader value={indexValue} perPage={loadPerPage} />}
+
+      <button
+            className='className="flex items-center cursor-pointer	bg-gray-100 hover:bg-gray-600 hover:text-white transition-colors duration-500 border border-gray-500 px-4 py-3"'
+            onClick={handleClick}
+            type="button"
+            >
+            Load More 
+            ({indexValue})
+            {console.log(loadedMoreData2)}
+            {/* {console.log(loadedData)} */}
+            
+        </button>
 
       <Footer footer={mainFooter}/>
     </Layout>
