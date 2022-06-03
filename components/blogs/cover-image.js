634dbaddf7caf436c324308d0107d86608a7c393
Kenneth Brenes <kenneth.brenes@Kenneth-Brenes-MacBook-Pro.local>
2022-06-03 11:47:00 -0600

    Continue with deployment clean up 2

--- components/blogs/cover-image.js
+++ components/blogs/cover-image.js
@@ -3,12 +3,14 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export default function CoverImage({ title, coverImage, slug }) {
+  
+  const backImage = (coverImage.node?.sourceUrl===undefined) ? "https://headlessgl22.wpengine.com/wp-content/uploads/2022/04/gl_blue.png" : coverImage.node?.sourceUrl
   const image = (
     <Image
       width={2000}
       height={1000}
       alt={`Cover Image for ${title}`}
-      src={coverImage?.node.sourceUrl}
+      src={backImage}
       className={cn('shadow-small', {
         'hover:shadow-medium transition-shadow duration-200': slug,
       })}
