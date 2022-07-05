3e9147e1d8b39b919d8e673344bc50993c2db742
Kenneth Brenes <kenneth.brenes@Kenneth-Brenes-MacBook-Pro.local>
2022-07-05 16:12:57 -0600

    Mobile main nav cleaned up, and contact us button relocated

--- components/layout/header.js
+++ components/layout/header.js
@@ -67,41 +67,32 @@ export default function Header(headerData) {
                 </div>
                    
             </div>
-            <div className="bg-gradient-to-t from-orange to-darkorange hover:text-darkblue px-4" >
-                <Link key="Contact-us"  href='/contact-us'>
-                        <a dangerouslySetInnerHTML={ { __html: "Contact Us" } }/>
-                </Link>
-            </div>
+            
         
             <a className="navbar-burger self-center mr-12 md:hidden" onClick={handleClick} href="#">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg> 
             </a> 
-        </nav>
-        {/* //Hamburguer menu */}
-        <div className={`${active ? '' : 'hidden'} md:hidden lg:hidden w-full lg:inline-flex lg:flex-grow lg:w-auto bg-lightblue`}>
-		    <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
-            { headerData.header.menuItems.nodes.length ? headerData.header.menuItems.nodes.map( menuItem => (
-                        
-                <Link key={ menuItem.id }  href={ menuItem.url ?? '/' }>
-                    <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white ' dangerouslySetInnerHTML={ { __html: menuItem.label } }/>
-                </Link>
-            ) ) : null }
-            <li className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white " key="mobile">
-                    <Link key="Blogs-mobile"  href='/Blogs'>
-                        <a dangerouslySetInnerHTML={ { __html: "Blogs" } }/>
-                    </Link> 
-                </li>
-                <li className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white " key="contact-mobile">
-                    <Link key="Contact-us-mobile" href='/contact-us'>
-                        <a dangerouslySetInnerHTML={ { __html: "Contact Us" } }/>
-                    </Link> 
-                </li>
-          </div>
-        </div>
         
-
+        {/* //Hamburguer menu */}
+            <div className={`${active ? '' : 'hidden'} md:hidden lg:hidden w-full lg:inline-flex lg:flex-grow lg:w-auto bg-lightblue`}>
+                <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
+                { headerData.header.menuItems.nodes.length ? headerData.header.menuItems.nodes.map( menuItem => (
+                            
+                    <Link key={ menuItem.id }  href={ menuItem.url ?? '/' }>
+                        <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white ' dangerouslySetInnerHTML={ { __html: menuItem.label } }/>
+                    </Link>
+                ) ) : null }
+            
+            </div>
+            </div>
+            <div className="bg-gradient-to-t from-orange to-darkorange hover:text-darkblue px-4 py-6" >
+                    <Link key="Contact-us"  href='/contact-us'>
+                            <a dangerouslySetInnerHTML={ { __html: "Contact" } }/>
+                    </Link>
+            </div>
+        </nav>
 
         <h1>{headerData.pageTitle}</h1>
 
