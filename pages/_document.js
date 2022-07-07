a908dca1a9ec3b96083cd677ecba8bf8ff370a5c
Kenneth Brenes <kenneth.brenes@Kenneth-Brenes-MacBook-Pro.local>
2022-07-07 12:11:16 -0600

    Keep working on the main nav mouse properties on child elements

--- pages/_document.js
+++ pages/_document.js
@@ -18,19 +18,19 @@ export default class MyDocument extends Document {
             crossOrigin="true"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Oxygen:wght@400;500&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Oxygen:wght@400;500"
             rel="stylesheet"
           />
           <link
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Oxygen:wght@400;500&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Oxygen:wght@400;500"
             media="print"
             onLoad="this.media='all'"
           />
           <link
             rel="preload"
             as="style"
-            href="https://fonts.googleapis.com/css2?family=Oxygen:wght@400;500&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Oxygen:wght@400;500"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Gulzar"
@@ -39,7 +39,7 @@ export default class MyDocument extends Document {
           <noscript>
             <link
               rel="stylesheet"
-              href="https://fonts.googleapis.com/css2?family=Oxygen:wght@400;500&display=swap"
+              href="https://fonts.googleapis.com/css2?family=Oxygen:wght@400;500"
             />
           </noscript>
         </Head>
