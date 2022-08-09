6b609fae67b76e310d7d71777036a4652312a1e6
Kenneth Brenes <kenneth.brenes@Kenneth-Brenes-MacBook-Pro.local>
2022-08-09 16:09:21 -0600

    Footer layout work around and some custom styles for embeded hs form

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -8,49 +8,43 @@ export default function Footer(headerData) {
     // console.log(headerData.footer.menuItems.nodes);
     // console.log("###");
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
+    <footer className="bg-accent-1 border-t border-accent-2 bg-gradient-to-t from-lightblue to-white text-darkblue w-screen py-5">
 
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          
-          <h3 className="text-1xl lg:text-1s  tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            WP example with graphql
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a href="">Item 1</a> | <a href="">Item 1</a>
+        <div className="grid grid-cols-3 gap-4">
+          <Script id="hs-legacy-forms-js" src="//js.hsforms.net/forms/v2-legacy.js" strategy="beforeInteractive" />
+          <Script id="hs-forms-js" src="//js.hsforms.net/forms/v2.js"  strategy="beforeInteractive"  onLoad={() =>hbspt.forms.create({})}/>   
+          <Script id="hbspt-footer" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `
+            hbspt.forms.create({
+              region: "na1",
+              portalId: "2059590",
+              formId: "5190425c-61e6-4ff3-bc6e-1d12786f903d",
+              target: '#footer-form'
+            });
+          `}} />
+
+          <div class="col-span-2 ...">
+            <div id="footer-form"></div>
+          </div>
+          <div class="...">
+            <h3 className="text-1xl lg:text-1s  tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+              WP example with graphql
+            </h3>
+            <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+              <a href="">Item 1</a> | <a href="">Item 1</a>
+            </div>
+          </div>
+          <div class="col-span-3 md:flex pt-4 content-center">
+            <ul className="list-reset flex justify-between flex-1 md:flex-none items-center">
+              { headerData.footer.menuItems.nodes.length ? headerData.footer.menuItems.nodes.map( menuItem => (
+                      
+                  <li className="hover:text-darkblue px-4" key={ menuItem.id }><Link  href={ menuItem.url ?? '/' }>
+                      <a dangerouslySetInnerHTML={ { __html: menuItem.label } }/>
+                  </Link></li>
+              )) : null }
+            </ul>
           </div>
         </div>
-       
-       <div>
-      <Script id="hs-legacy-forms-js" src="//js.hsforms.net/forms/v2-legacy.js" strategy="beforeInteractive" />
-    
-      <Script id="hs-forms-js" src="//js.hsforms.net/forms/v2.js"  strategy="beforeInteractive"  onLoad={() =>hbspt.forms.create({})}/>   
-      
-      <Script id="hbspt-footer" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `
-        hbspt.forms.create({
-          region: "na1",
-          portalId: "2059590",
-          formId: "5190425c-61e6-4ff3-bc6e-1d12786f903d",
-          target: '#footer-form'
-        });
-      `}} />
-            
-      <div id="footer-form"></div>
-      </div>
-
-      
-        <div className="hidden md:flex w-3/4 pt-2 content-center justify-between md:justify-end ">
-        <ul className="list-reset flex justify-between flex-1 md:flex-none items-center">
-                { headerData.footer.menuItems.nodes.length ? headerData.footer.menuItems.nodes.map( menuItem => (
-                        
-                        <li className="hover:text-darkblue px-4" key={ menuItem.id }><Link  href={ menuItem.url ?? '/' }>
-                            <a dangerouslySetInnerHTML={ { __html: menuItem.label } }/>
-                        </Link></li>
-                    ) ) : null }
-                        
-                </ul>
-            </div>
-
 
       </Container>
     </footer>
