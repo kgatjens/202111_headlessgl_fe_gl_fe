319ec4077921b0fef7bc7e23163c9ed8cb708125
Kenneth Brenes <kenneth.brenes@Kenneth-Brenes-MacBook-Pro.local>
2022-09-01 15:02:42 -0600

    Search: Styles keep working on the search on the main nav positioning - logic

--- components/search/search-form.js
+++ components/search/search-form.js
@@ -11,7 +11,7 @@ const SearchForm = ( {searchQuery, setSearchQuery, handleSearchFormSubmit} ) => 
 
   return (
     <form className="flex w-full justify-center" onSubmit={handleSearchFormSubmit}>
-      <div className="block relative w-4/5 group">
+      <div className="block  w-4/5 group">
            
         <input
           placeholder="Search..."
@@ -19,16 +19,14 @@ const SearchForm = ( {searchQuery, setSearchQuery, handleSearchFormSubmit} ) => 
           id="search-nav-input"
           aria-label="Search" aria-describedby="button-addon2"
           onChange={( event ) => setSearchQuery( event.target.value )}
-           className="w-screen -left-55 absolute bg-gray3 top-16 text-center text-white transition z-10 focus:outline-none focus:border-transparent p-2 appearance-none leading-normal text-xl lg:text-2xl placeholder:text-gray2 placeholder:pl-[54px]"
-          //className="peer cursor-pointer relative z-10 h-12 w-12 rounded-full border bg-transparent pl-12 outline-none focus:w-full focus:cursor-text focus:border-lime-300 focus:pl-16 focus:pr-4"
+           className="w-screen -left-55 absolute bg-gray3 top-20 text-center text-white transition z-10 focus:outline-none focus:border-transparent p-2 appearance-none leading-normal text-xl lg:text-2xl placeholder:text-gray2 placeholder:pl-[54px]"
           />
-          <b x-show="search.trim() != '''"
-                className="text-white hover:text-darkblue cursor-pointer text-lg absolute top-16 right-2" title="clear" />
-                <span>&times;</span>
-      
-      <svg onClick={handleSearchFormSubmit} onMouseMove={(e) => handleSearchHover(e)} className="  block w-10 p-2 group-hover:block" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search"  role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000">
-          <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
-      </svg>
+
+        <svg onClick={(e) => handleSearchHover(e)} onMouseMove={(e) => handleSearchHover(e)} className="h-5 w-5 fill-current" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30"
+                      height="30" viewBox="0 0 30 30">
+            <path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971  23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z">
+            </path>
+        </svg>
       </div>
       
     </form>
