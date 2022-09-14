e7c5ab73b442ce49c914647bf35ab0b052947c1d
Kenneth Brenes <kenneth.brenes@Kenneth-Brenes-MacBook-Pro.local>
2022-09-13 19:28:34 -0600

    Forms: Installed react package to test hubspot forms

--- components/forms/hubspot/contact-us.js
+++ components/forms/hubspot/contact-us.js
@@ -1,4 +1,6 @@
 import Script from 'next/script'
+import HubspotForm from 'react-hubspot-form'
+
 
 export default function ContactUsForm() {
     
@@ -6,18 +8,17 @@ export default function ContactUsForm() {
   return (
     <>
       <div className="sm:mx-0">
-        {/* <Script id="hs-legacy-forms-js" src="//js.hsforms.net/forms/v2-legacy.js" strategy="beforeInteractive" />
-        <Script id="hs-forms-js" src="//js.hsforms.net/forms/v2.js"  strategy="beforeInteractive"  onLoad={() =>hbspt.forms.create({})}/>    */}
-        <Script id="hbspt-contactus" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `
-          hbspt.forms.create({
-            region: "na1",
-            portalId: "2059590",
-            formId: "9a3b6713-d4c3-44c8-a450-2ba09b052e8b",
-            target: '#contact-us-form'
-          });
-        `}} />  
+         
 
-        <div id='contact-us-form'></div>
+
+
+<HubspotForm id="footer-hs-form"
+              portalId='2059590'
+              formId='9a3b6713-d4c3-44c8-a450-2ba09b052e8b'
+              onSubmit={() => console.log('Submit!')}
+              onReady={(form) => console.log('Form ready!')}
+              loading={<div>Loading...</div>}
+              />
       </div>
     </>
       
