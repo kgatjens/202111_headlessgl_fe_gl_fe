2eb77e91e79bdf691b3e8f63a4e57195926a2b20
Kenneth Brenes <kenneth.brenes@Kenneth-Brenes-MacBook-Pro.local>
2022-09-20 16:36:03 -0600

    Util: started to define the structure and test to kepp UTM params in the url if isset

--- next.config.js
+++ next.config.js
@@ -11,74 +11,74 @@ module.exports = {
       locales: ['en'],
       defaultLocale: 'en',
     },
-    generateUrlWithQueryString,
-    getAllowedParams,
-    prepareParamsAsQueryString,
-    filterAllowedParams
+    // generateUrlWithQueryString,
+    // getAllowedParams,
+    // prepareParamsAsQueryString,
+    // filterAllowedParams
 
 }
 
 
-/* Function to persist URL params ( utms )*/ 
-function getAllowedParams() {
-   const allowedParams = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content', 'mid', 'gclid', 'source'];
+// /* Function to persist URL params ( utms )*/ 
+// function getAllowedParams() {
+//    const allowedParams = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content', 'mid', 'gclid', 'source'];
 
-   return allowedParams;
-}
+//    return allowedParams;
+// }
 
-function prepareParamsAsQueryString() {
-   const params = getAllowedParams();
-   let paramsLikeQueryString = [];
+// function prepareParamsAsQueryString() {
+//    const params = getAllowedParams();
+//    let paramsLikeQueryString = [];
 
-   for (let index = 0; index < params.length; index++) {
-       const param = params[index];
+//    for (let index = 0; index < params.length; index++) {
+//        const param = params[index];
 
-       paramsLikeQueryString.push(`${param}=:${param}?`);
-   }
+//        paramsLikeQueryString.push(`${param}=:${param}?`);
+//    }
 
-   return paramsLikeQueryString.join('&');
-}
+//    return paramsLikeQueryString.join('&');
+// }
 
-function generateUrlWithQueryString(url, params) {
-   if(Object.keys(params).length) {
-       const filteredParams = filterAllowedParams(params);
+// function generateUrlWithQueryString(url, params) {
+//    if(Object.keys(params).length) {
+//        const filteredParams = filterAllowedParams(params);
 
-       if (Object.keys(filteredParams).length) {
-           if(url[0] != '/')
-               url = `/${url}`;
+//        if (Object.keys(filteredParams).length) {
+//            if(url[0] != '/')
+//                url = `/${url}`;
 
-           return `${url}?${serialize(filteredParams)}`;
-       }
+//            return `${url}?${serialize(filteredParams)}`;
+//        }
 
-       return url;
-   }
+//        return url;
+//    }
 
-   return url;
-}
+//    return url;
+// }
 
-function filterAllowedParams(params) {
-   if(Object.keys(params).length) {
-       const filteredParams = Object.keys(params)
-           .filter(key => getAllowedParams().includes(key.toLowerCase()))
-           .reduce((obj, key) => {
-               obj[key] = params[key];
-               return obj;
-           }, {});
+// function filterAllowedParams(params) {
+//    if(Object.keys(params).length) {
+//        const filteredParams = Object.keys(params)
+//            .filter(key => getAllowedParams().includes(key.toLowerCase()))
+//            .reduce((obj, key) => {
+//                obj[key] = params[key];
+//                return obj;
+//            }, {});
 
-       return filteredParams;
-   }
+//        return filteredParams;
+//    }
 
-   return params;
-}
+//    return params;
+// }
 
-// INTERNAL
-function serialize(obj) {
-   var str = [];
-   for (var p in obj) {
-       if (obj.hasOwnProperty(p)) {
-           str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-       }
-   }
+// // INTERNAL
+// function serialize(obj) {
+//    var str = [];
+//    for (var p in obj) {
+//        if (obj.hasOwnProperty(p)) {
+//            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+//        }
+//    }
 
-   return str.join("&");
-}
+//    return str.join("&");
+// }
