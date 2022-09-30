cd8a82800b67d4a7843d3266b557d290659bed60
Kenneth Brenes <kenneth.brenes@Kenneth-Brenes-MacBook-Pro.local>
2022-09-30 15:45:51 -0600

    Clock component: Added module, component, and installed in the index page. Old clocks not used.

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import Layout from '../components/layout/layout'
 import Header from '../components/layout/header'
 import Footer from '../components/layout/footer'
 
+import Clocks from '../components/widgets/clocks'
+
 import Homepage from '../components/pages/homepage'
 
 import { getMenus,getHomepage,getHomepageCarousel } from '../lib/wp/api'
@@ -55,7 +57,7 @@ export default function Home({ data, homepage, carouselHomepage  }) {
 
       
       </Container>
-    
+      <Clocks/>
       <Footer footer={mainFooter}/>
     </Layout>
     </>
