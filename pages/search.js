e42132544a765b8c225cafffd34ca7b7abe2aa85
Kenneth Brenes <kenneth.brenes@gorillalogic.com>
2023-03-06 16:12:42 -0600

    Test for strapi fuzzy consumed search api: ok

--- pages/search.js
+++ pages/search.js
@@ -25,7 +25,8 @@ export default function Search({ menus }) {
   
   const searchQueryString = process.browser ? ( Router?.query?.s ?? '' ) : '';//query string
 
-  const address = `https://headlessgl22.wpengine.com/wp-json/wp/v2/search/?per_page=20&subtype=page&subtype=post&search=`+searchQueryString;
+  //const address = `https://headlessgl22.wpengine.com/wp-json/wp/v2/search/?per_page=20&subtype=page&subtype=post&search=`+searchQueryString;
+  const address = `http://localhost:1337/api/fuzzy-search/search?query=back-end`+searchQueryString;
   const fetcher = async (url) => await axios.get(url).then((res) => res.data);
   const { data, error } = useSWR(address, fetcher);
 
@@ -40,6 +41,8 @@ export default function Search({ menus }) {
 
   const metaData = {metaTitle,featuredImage,metaKeywords,metaDesc,canonical}
 
+
+  console.log(data);
   return (
     <>
       <Layout>
@@ -54,19 +57,24 @@ export default function Search({ menus }) {
 
       <ul role="list" className="flex flex-col p-4">
         {data &&
-          data.map((item) => (
-            <li className="border-gray-400 flex flex-row mb-2" key={item.id} >
-              <div className="select-none flex flex-1 items-center p-4 transition duration-500 ease-in-out transform hover:-translate-y-2 rounded-2xl border-2 p-6 hover:shadow-2xl border-green">
-                <div className="flex-1 pl-1 mr-16">
-                    <div className="font-medium">
-                      <h3 className="text-darkblue">{`${item.title}`}</h3>
-                    </div>
-                </div>
-                <div className="w-1/4 text-wrap text-center flex text-white text-bold flex-col rounded-md bg-darkorange justify-center items-center mr-10 p-2">
-												<a href="">View</a>
-											</div>
-              </div>
-            </li>
+          data.articles.map((id,item) => (
+            <>
+            {console.log(item)}
+            <li key={id} className="ml-4 font-normal">
+                <p>{id.title}</p>
+              </li>
+          
+            </>
+          ))}
+           {data &&
+          data.capabilities.map((id,item) => (
+            <>
+            {console.log(item)}
+            <li key={id} className="ml-4 font-normal">
+                <p>{id.HeaderMainTitle}</p>
+              </li>
+          
+            </>
           ))}
       </ul>
       </div>
