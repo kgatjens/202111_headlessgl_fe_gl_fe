876708f8cebbd6d144fe8a06cf7cc35d9d237e95
kbrenes <kenneth@Macbooks-MacBook-Pro.local>
2023-05-03 16:37:33 -0600

    Keep working on Prismic setup

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,15 @@ import Head from 'next/head'
 //import 'tailwindcss/tailwind.css'
 import HeadTracking from '../components/tracking/head-tracking'
 import {ThemeProvider} from 'next-themes'
+import Link from 'next/link'
+import { PrismicProvider } from '@prismicio/react'
+import { PrismicPreview } from '@prismicio/next'
+import { repositoryName } from '../prismicio'
 
 function MyApp({ Component, pageProps }) {
 
   return <>
-  <ThemeProvider attribute="class">
+  {/* <ThemeProvider attribute="class">
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
     </Head>
@@ -17,7 +21,13 @@ function MyApp({ Component, pageProps }) {
 
     
     <Component {...pageProps} />
-  </ThemeProvider>
+    
+  </ThemeProvider> */}
+  <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
+      <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
 
   
   </>
